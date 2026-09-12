6d807c3c5f60b7e7e83cdc138b4b17ce5f32136b
Chris Burrell <chrisburrell@gmail.com>
2026-09-11 20:28:05 -0500

    feat: rotation controller with Framer Motion transitions

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import AutoRefresh from "./components/AutoRefresh";
 import Menu from "./components/Menu";
+import Rotation from "./components/Rotation";
 
 export default function Home() {
   return (
     <>
-      <Menu />
+      <Rotation>
+        <Menu />
+      </Rotation>
       <AutoRefresh buildId={__BUILD_ID__} />
     </>
   );
